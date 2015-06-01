b710e61ef2b12df931795eab1b0db4bce3fe9ac8
intelatrix <mrallson@hotmail.com>
2015-06-01 17:42:36 +0800

    Based Set Up for more
    Sound Manager Created
    Game Name and Company Changed
    Google Play Set-up

---
+++ FYP2_Framework - Battle Scroller/Assets/Scripts/Framework/Managers/SoundManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour
+{
+	public enum Sounds
+	{
+		MAIN_BGM
+	}	
+	public static SoundManager Instance { get; private set; }
+	
+	
+	void Awake()
+	{
+		Instance = this;
+		Object.DontDestroyOnLoad(this.gameObject);
+	}
+	
+	public void PlaySound(Sounds SoundType)
+	{
+	
+	
+	}
+}
+
